6ee8958eca7de3dd53e9469cb912bcb8a907d03a
Molinas26 <85413135+Molinas26@users.noreply.github.com>
2021-10-30 15:26:38 -0600

    Cambio Vol6

--- routes/web.php
+++ routes/web.php
@@ -177,11 +177,11 @@ Route::middleware("auth")
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 
 
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
